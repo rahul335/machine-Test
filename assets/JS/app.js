0ca40e61b224d6e646b50dc2bef337073333f8ad
rahul335 <74925492+rahul335@users.noreply.github.com>
2023-08-08 02:06:07 +0530

    work on js functinality

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,23 +1,29 @@
 let cl = console.log;
 
-const bundlesSection = [...document.querySelectorAll('.bundles-section')];
-
-const pair = [... document.querySelectorAll('.pair')];
-// const radiobtn = [...document.querySelectorAll('.radio')];
+const pair = [...document.querySelectorAll('.pair')];
+const radiobtn = [...document.querySelectorAll('.radio')];
 
 
-
-const onhandler = (eve) =>{
-    bundlesSection.forEach((eves) => {
-        eves.classList.remove('active')
+const onhandler = (eve) => {
+    pair.forEach((eves) => {
+        eves.parentElement.classList.remove('active');
+        eves.children[0].classList.remove('active');
     })
-    eve.target.classList.add('active');
+    if (eve.target.matches('.pair')) {
+        eve.target.parentElement.classList.add('active');
+        eve.target.children[0].classList.add('active');
+    }else{
+        eve.target.closest(".pair").parentElement.classList.add('active');
+        eve.target.closest(".pair").children[0].classList.add('active');
+    }
+
+
 }
 
 
 
 
-bundlesSection.forEach((ele)=>{
+pair.forEach((ele) => {
     ele.addEventListener('click', onhandler)
 })
 
